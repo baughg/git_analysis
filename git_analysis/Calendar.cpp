0aa9bfc2d80641e63da294d9fa873a203232a061
Gary Baugh <baughg@tcd.ie>
2020-06-07 18:07:56 +0100

    Generating ww lookup

--- git_analysis/Calendar.cpp
+++ git_analysis/Calendar.cpp
@@ -1,5 +1,6 @@
 #include "Calendar.h"
 #include <cassert>
+#include <vector>
 
 using namespace GB;
 
@@ -9,17 +10,17 @@ Calendar::Calendar(uint32_t year)
 	generate();
 }
 
-
 Calendar::Day Calendar::day_of_month(const uint32_t &day, const Month &mon)
 {
 	const uint32_t month{ 
-		static_cast<uint32_t>(mon) - static_cast<uint32_t>(Month::january) };
+		static_cast<uint32_t>(mon) };
 
+	uint32_t year{ year_ };
 	static uint32_t t[] = { 0, 3, 2, 5, 0, 3, 5, 1,
 					   4, 6, 2, 4 };
-	year_ -= month < 3;
-	const uint32_t day_index{ (year_ + (year_ >> 2) - year_ / 100 +
-		year_ / 400 + t[month - 1] + day) % 7 };
+	year -= month < 3;
+	const uint32_t day_index{ (year + (year >> 2) - year / 100 +
+		year / 400 + t[month - 1] + day) % 7 };
 
 	return static_cast<Day>(static_cast<uint32_t>(Day::sunday) + day_index);
 }
@@ -54,9 +55,49 @@ uint32_t Calendar::number_of_days(const Month &month)
 	return days;
 }
 
-void Calendar::generate() {
-	for (Month month{ Month::january }; month <= Month::december;
-		month++) {
+Calendar::Month &operator++(Calendar::Month &e)
+{
+	if (e == Calendar::Month::december) {
+		e = Calendar::Month::january;
+		return e;
+	}
+	e = Calendar::Month(static_cast<std::underlying_type<Calendar::Month>::type>(e) + 1);
+	return e;
+}
 
+void Calendar::generate() {
+	const static std::vector<Month> months{
+		Month::january,
+		Month::february,
+		Month::march,
+		Month::april,
+		Month::may,
+		Month::june,
+		Month::july,
+		Month::august,
+		Month::september,
+		Month::october,
+		Month::november,
+		Month::december
+	};
+
+	uint32_t week{ 0 };
+	const Day first_day_of_year{ day_of_month(1,Month::january) };
+	week += static_cast<uint32_t>(first_day_of_year != Day::sunday);	
+
+	for (auto month : months) {
+		const uint32_t days{ number_of_days(month) };
+		const uint32_t month_key{ static_cast<uint32_t>(month) << 16 };
+
+		for (uint32_t day{ 1 }; day <= days; ++day) {
+			Day day_of_week{ day_of_month(day,month) };
+
+			if (day_of_week == Day::sunday) {
+				week++;
+			}
+
+			const uint32_t month_day_key{ month_key | day };
+			week_lut_[month_day_key] = week;
+		}
 	}
 }
--- git_analysis/Calendar.h
+++ git_analysis/Calendar.h
@@ -30,27 +30,17 @@ namespace GB {
 			september,
 			october,
 			november,
-			december	
+			december			
 		};
-
 		
-
-		Calendar() = default;
+		Calendar() = default;		
 		explicit Calendar(uint32_t year);
 		uint32_t number_of_days(const Month &month);
 		Calendar::Day day_of_month(const uint32_t &day, const Month &month);
 	private:
 		uint32_t year_{};
 		void generate();
-	};
-
-	Calendar::Month& operator++(Calendar::Month& e)
-	{
-		if (e == Calendar::Month::december) {
-			throw std::out_of_range("for Month& operator ++ (Month&)");
-		}
-		e = Calendar::Month(static_cast<std::underlying_type<Calendar::Month>::type>(e) + 1);
-		return e;
-	}
+		std::map<uint32_t, uint32_t> week_lut_;
+	};	
 }
 
