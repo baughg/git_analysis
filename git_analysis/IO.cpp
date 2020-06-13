7d9b689581283115b182e8689ae2b1911f48e389
Gary Baugh <baughg@tcd.ie>
2020-06-13 23:29:51 +0100

    Add IO class

--- git_analysis/IO.cpp
+++ git_analysis/IO.cpp
@@ -3,6 +3,28 @@
 
 using namespace GB;
 
+bool IO::read_lines(
+	const std::string &filename,
+	uint32_t &line_count,
+	uint32_t &char_count) {
+	std::ifstream text_file(filename.c_str());
+	line_count = 0;
+	char_count = 0;
+
+	if (!text_file.good()) {
+		return false;
+	}
+
+	while (!text_file.eof()) {
+		std::string line_in{};
+		getline(text_file, line_in);
+		line_count++;
+		char_count += static_cast<uint32_t>(line_in.length());
+	}
+
+	return line_count > 0;
+}
+
 bool IO::read_text(
 	const std::string &filename, std::deque<std::string> &lines)
 {
--- git_analysis/IO.h
+++ git_analysis/IO.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <string>
 #include <deque>
 
@@ -8,7 +9,12 @@ namespace GB {
 	public:
 		static bool read_text(
 			const std::string &filename, 
-			std::deque<std::string> & lines);
+			std::deque<std::string> &lines);
+
+		static bool read_lines(
+			const std::string &filename,
+			uint32_t &line_count,
+			uint32_t &char_count);
 	};
 }
 
